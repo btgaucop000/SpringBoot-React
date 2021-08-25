f8e85f1cfa625fbd115b2842e1b91db408f21c5c
DESTINY-LAPTOP\ADMIN <tabaotien1997@gmail.com>
2021-08-25 22:57:22 +0700

    feat: connect db and creat api get all employees

---
+++ springboot-backend/src/main/java/com/bfdestiny/springboot/model/Employee.java
@@ -0,0 +1,27 @@
+package com.bfdestiny.springboot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "mail_address")
+    private String mailAddress;
+}
